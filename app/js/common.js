002094005266ff9ab97c283c77fc56c4a309b729
enotik <woaouh@outlook.com>
2019-11-24 23:52:56 +0200

    VAULT-TEC repository has created

---
+++ app/js/common.js
@@ -0,0 +1,9 @@
+$(function() {
+
+	$('.main-nav-wrapper').after('<div class="mobile-menu d-lg-none">');
+	$('.top-menu').clone().appendTo('.mobile-menu');
+	$('.mobile-menu-button').click(function() {
+		$('.mobile-menu').stop().slideToggle();
+	});
+
+});
